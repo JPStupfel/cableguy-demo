1fec203c3a08db0c451ca06600df369e56e1ebea
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-09-23 11:23:51 -0500

    del lock in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.15.0"
+# lock "~> 3.15.0"
 
 set :application, "cableguy"
 set :repo_url, "git@github.com:JPStupfel/cableguy-demo.git"
